cd11ce77fb173b8627dd037c1c0379ca953edd7d
sven <sven@svtsc.de>
2021-02-01 00:25:20 +0100

    Add function diskspace_check which which finds the percentage of used diskspace and compares it with treshold values

--- report.sh
+++ report.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
 # See README.md
 
-# Global_Vars
+### Global_Vars
+# Mailserver hostname or address
 SMTPSERVER=Mailserver
 # Warning treshold percentage for free disk space
-DISKSPACE_WARN=Number
-#The Warnlevel for the e-mail subject (Can be info, warning, critical)
-WARNLEVEL=info
+DISKSPACE_WARN=80
+# Critical treshold percentage for free disk space
+DISKSPACE_CRIT=80
 
-df_file="./df.txt"
+#Warnlevels for the e-mail subject (Can be 0=info, 1=warning, 2=critical)
+WARNLEVEL=0
+
+
+DF_FILE="./df.txt"
 
 write_diskspace() {
-	df -h > $df_file
+	df -h > $DF_FILE
 }
 
-#diskspace_check() {
-#Checks for the free space values in $df_file
-#}
-
-
-#line by line
-#count from 100% to zero or reverse?
+diskspace_check() {
+	#Checks for the free space values in $DF_FILE
+	while read LINE; do
+		POS_PERCENT=`expr index "$LINE" %`
+		PERCENT=${LINE:$POS_PERCENT-4:3}
+		echo $PERCENT
+		
+		if (($PERCENT>=DISKSPACE_WARN));
+			then
+			if (($WARNLEVEL<1));
+				then
+				WARNLEVEL=1
+				fi
+			fi
+			
+		if (($PERCENT>=DISKSPACE_CRIT));
+			then
+			if (($WARNLEVEL<2));
+				then
+				WARNLEVEL=2
+				fi
+			fi
+			
+	done < $DF_FILE
+}
 #write line for mail.txt file, which is the content of the later sent mail
 
 write_diskspace
+diskspace_check
+echo "Warnlevel = $WARNLEVEL"
+ 
