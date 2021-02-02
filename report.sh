c90eadd472b2013d0b2ff199c14ab381fbd60f38
sven <sven@svtsc.de>
2021-02-02 11:35:23 +0100

    Add variable for script-path to avoid problems when running by cronjob

--- report.sh
+++ report.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # See README.md
 
-source config.sh
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source $SCRIPTPATH/config.sh
 
 #Warnlevels for the e-mail subject (Can be 0=info, 1=warning, 2=critical)
 WARNLEVEL=0
