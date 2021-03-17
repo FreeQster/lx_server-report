e778cdc29f770c8a9ea021d1e5af7f79975ee1f5
Sven Tschoepe <sven@svtsc.de>
2021-03-18 00:45:50 +0100

    Dot-source functions.sh in report.sh

--- report.sh
+++ report.sh
@@ -3,6 +3,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $SCRIPTPATH/config.sh
+. $SCRIPTPATH/functions.sh
 
 #Warnlevels for the e-mail subject (Can be 0=info, 1=warning, 2=critical)
 WARNLEVEL=0
