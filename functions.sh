3702a6270fe858b781d08857b05dcab3f0c48e98
Sven Tschoepe <sven@svtsc.de>
2021-03-18 01:00:39 +0100

    Move functions from install.sh to functions.sh

--- functions.sh
+++ functions.sh
@@ -1,3 +1,52 @@
+### install.sh
+root_check(){
+  # Check if the script was started as root
+  USER_ID=$(id -u)
+  if [[ $USER_ID != "0" ]];
+  then
+    echo "You are not root - EXITING"
+    exit 1
+  fi
+}
+
+choose_path(){
+  # Configure install path
+  echo "Please enter install path (Default=/sc/lx_server-report):"
+  read $INSTALL_PATH
+
+  if [ -z "$INSTALL_PATH" ];
+  then
+    INSTALL_PATH='/sc/lx_server-report'
+  fi
+}
+
+get_files(){
+  # Download and prepare the script files
+  wget -O /tmp/lx_server-report.zip https://github.com/FreeQster/lx_server-report/archive/main.zip
+  unzip -d /tmp/ /tmp/lx_server-report.zip
+
+  # Create install dir
+  mkdir -p $INSTALL_PATH
+
+  # Copy files
+  cp /tmp/lx_server-report-main/report.sh $INSTALL_PATH/
+  cp /tmp/lx_server-report-main/README.md $INSTALL_PATH/
+  cp /tmp/lx_server-report-main/example_config.sh $INSTALL_PATH/
+  cp $INSTALL_PATH/example_config.sh $INSTALL_PATH/config.sh
+
+  # Make the script executable, just to be sure
+  chmod +x $INSTALL_PATH/report.sh
+}
+
+add_cron(){
+  # Adds a cron file in /etc/cron.d, we want the script to run every 5 minutes
+  touch lx_server-report_cron
+  echo "# Entry for the lx_server-report script in $INSTALL_PATH" | tee -a lx_server-report_cron
+  echo "*/5 * * * * root  /bin/bash -c "$INSTALL_PATH/report.sh"" | tee -a lx_server-report_cron
+  mv -f ./lx_server-report_cron /etc/cron.d/lx_server-report
+}
+
+### report.sh
 write_diskspace() {
 	df -h > $DF_FILE
 }
