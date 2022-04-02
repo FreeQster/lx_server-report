989c85eb1410dc7200b63820164e6e6e984566cb
Sven Tschoepe <sven@svtsc.de>
2022-04-02 21:08:44 +0200

    Edit example_config.sh

--- example_config.sh
+++ example_config.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Mailserver hostname or address
+SMTPSERVER=smtp_server
 # Recipient e-mail address
-MAIL_TO=root
+MAIL_TO=address@domain.tld
 
 # Warning treshold percentage for free disk space
 DISKSPACE_WARN=80
